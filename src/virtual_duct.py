acad64bfca02c78dde54419825af3308cca23295
artdeheer <art@deheer.de>
2026-05-04 13:25:29 +0200

    virtual_duct implementation

--- src/virtual_duct.py
+++ src/virtual_duct.py
@@ -11,20 +11,25 @@ class VirtualDuct:
         # We model this as a small FIR filter representing speaker response
         self.s_truth = np.exp(-0.1 * np.arange(10)) * np.sin(2 * np.pi * 0.1 * np.arange(10))
 
+        self.L = len(self.s_truth)
     def simulate_step(self, x_n, y_n):
         """
         Input: x_n (source noise), y_n (anti-noise from speaker)
         Output: d_n (delayed noise at mic), e_n (error mic reading)
         """
-        # 1. Math for the Primary Path (P): Shift samples through the pipe
-        # Logic: Push x_n into buffer, pop d_n from the end
-        
-        # 2. Math for the Secondary Path (S): Anti-noise traveling to mic
-        # Logic: Push y_n into buffer, pop delayed version, then convolve with s_truth
-        
-        # 3. Acoustic Summation: e(n) = d(n) + y'(n)
 
-        d_n = 0
-        e_n = 0
+        # PRIMARY PATH
+        d_n_pure = self.buffer_P[-1]
+        d_n = d_n_pure * 0.95 # sound dissapates as it travels
+
+        self.buffer_P[1:] = self.buffer_P[:-1] # shift all samples to the right
+        self.buffer_P[0] = x_n # insert at start
+
+        # SECONDARY PATH
+        self.buffer_S[1:] = self.buffer_S[:-1]
+        self.buffer_S[0] = y_n
+
+        y_prime = np.dot(self.buffer_S[:self.L], self.s_truth)
+        e_n = d_n + y_prime
         
         return d_n, e_n
